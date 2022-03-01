8444daf889ad7d8a048b3cbf3ff1726434b53668
Kevin Zheng <90434884+K-L-Zheng@users.noreply.github.com>
2022-02-28 23:43:41 -0500

    Add files via upload

--- WordleClone/wordleclone.js
+++ WordleClone/wordleclone.js
@@ -3,7 +3,9 @@ let column = 0;
 let activeBox = document.querySelector("#box" + row + column + " input");
 let validWords = ["sword", "store", "pause", "graph"];
 let validAns = ["sword", "pause"];
-let answer = "sword";
+let answer = ["s","w", "e", "e", "t"];
+let matchedLetters = ["-", "-", "-", "-", "-"];
+let keyboard = ["a", "b", "c", "d", "e", "f", "g" , "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 activeBox.focus();
 activeBox.onkeyup = nextInput;
@@ -21,7 +23,7 @@ function nextInput() {
 document.addEventListener("keyup", function(keyPress) {
     if (keyPress.code === "Enter") {
         let attempt = document.querySelector("#box00 input").value + document.querySelector("#box01 input").value + document.querySelector("#box02 input").value + document.querySelector("#box03 input").value + document.querySelector("#box04 input").value;
-        if (attempt === answer) {
+        if (attempt === answer.join("")) {
             document.querySelector("#box00 input").style.backgroundColor = "green";
             document.querySelector("#box01 input").style.backgroundColor = "green";
             document.querySelector("#box02 input").style.backgroundColor = "green";
@@ -30,16 +32,24 @@ document.addEventListener("keyup", function(keyPress) {
         }
         else {
             for (let i = 0; i < 5; i++) {
-                for (let x of answer) {
-                    if (attempt[i] === x) {
-                        if (attempt[i] === answer[i]) {
-                            document.querySelector("#box0" + i + " input").style.backgroundColor = "green";
-                            break;
-                        }
-                        else {
-                            document.querySelector("#box0" + i + " input").style.backgroundColor = "yellow";
-                            break;
-                        }
+                if (answer.includes(attempt[i])) {
+                    if (attempt[i] === answer[i]) {
+                        document.querySelector("#box0" + i + " input").style.backgroundColor = "green";
+                        answer.splice(i, 1, "-"); // placeholder hyphen for spliced letter so the # of elements in the array remains the same
+                    }
+                    else {
+                        matchedLetters.splice(i, 1, attempt[i]);
+                    }
+                }
+                else {
+                    document.querySelector("#box0" + i + " input").style.backgroundColor = "red";
+                }
+            }
+            for (let i = 0; i < 5; i++) {
+                if (matchedLetters[i].match(/[A-z]/)) { //checks to see if the element is a letter
+                    if (answer.includes(matchedLetters[i])) {
+                        document.querySelector("#box0" + i + " input").style.backgroundColor = "yellow";
+                        answer.splice(answer.indexOf(matchedLetters[i]), 1, "-");
                     }
                     else {
                         document.querySelector("#box0" + i + " input").style.backgroundColor = "red";
