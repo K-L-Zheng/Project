5729cb0cc56d1e6abbce8769770c0cb3ba7fe34f
Kevin Zheng <90434884+K-L-Zheng@users.noreply.github.com>
2022-03-05 01:10:24 -0500

    Add files via upload

--- WordleClone/wordleclone.js
+++ WordleClone/wordleclone.js
@@ -1,99 +1,110 @@
 let row = 0;
 let column = 0;
-let validWords = ["sword", "store", "pause", "graph", "sweet", "seeds", "speed", "sbmad", "brand", "swift", "shift", "dance"];
-let validAns = ["sword", "pause", "sweet", "speed", "sbmad", "swift", "shift", "dance"];
+const validWords = ["sword", "store", "pause", "graph", "sweet", "seeds", "speed", "sbmad", "brand", "swift", "shift", "dance"];
+const validAns = ["sword", "pause", "sweet", "speed", "sbmad", "swift", "shift", "dance"];
 let answer = "swift";
 // let keyboard = ["a", "b", "c", "d", "e", "f", "g" , "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let gameWon = false;
 
 document.addEventListener("keydown", function(keypress) {
-    if (/[A-z]/.test(keypress.key) && /[^\\\][^_]/.test(keypress.key)) { //filters out numbers + special characters, second part filters out [, ], \, ^, _
-        if (keypress.key.length === 1 && column < 5) { //prevent non-letter keys from changing input focus & prevents input for last box to be changed once a letter is entered
-            document.querySelector("#box" + row + column + " input").focus();
-            document.activeElement.value = "";
-            column += 1; //column needs to reach 5 in order for backspace to function properly, otherwise backspace will clear the 4th box instead of the 5th
-        }
-        else if (keypress.key === "Backspace") { //prevents backspace from deleting the value in the previous box
-            keypress.preventDefault();
-        }
-    }
-    else {
-        keypress.preventDefault(); //prevents key event from activating
-    }
-
-    if (keypress.code === "Backspace") {
-        if (column !== +document.activeElement.parentElement.id[4]) {
-            column -= 1;
-            document.querySelector("#box" + row + column + " input").value = "";
-            if (column > 0) {
-                document.querySelector("#box" + row + (column - 1) + " input").focus();
+    if (gameWon === false) {
+        if (/[A-Za-z]/.test(keypress.key)) { //filters out numbers + special characters
+            if (keypress.key.length === 1 && column < 5) { //prevent non-letter keys from changing input focus & prevents input for last box to be changed once a letter is entered
+                document.querySelector("#box" + row + column + " input").focus();
+                document.activeElement.value = "";
+                column += 1; //column needs to reach 5 in order for backspace to function properly, otherwise backspace will clear the 4th box instead of the 5th
             }
-            else {
+            else if (keypress.key === "Backspace" || keypress.key === "Tab") { //prevents backspace from deleting the value in the previous box & tab from moving the focus to another box
+                keypress.preventDefault();
+            }
+        }
+        else {
+            keypress.preventDefault(); //prevents key event from activating
+        }
+
+        if (keypress.code === "Backspace") {
+            if (column !== +document.activeElement.parentElement.id[4]) {
+                column -= 1;
+                document.querySelector("#box" + row + column + " input").value = "";
+                if (column > 0) {
+                    document.querySelector("#box" + row + (column - 1) + " input").focus();
+                }
+                else {
+                    document.querySelector("#box" + row + column + " input").focus();
+                }
+            }
+            else { //for when clicks reassign the column
+                document.querySelector("#box" + row + column + " input").value = "";
+                column = column > 0 ? column - 1 : column;
                 document.querySelector("#box" + row + column + " input").focus();
             }
         }
-        else { //for when clicks reassign the column
-            document.querySelector("#box" + row + column + " input").value = "";
-            column = column > 0 ? column - 1 : column;
-            document.querySelector("#box" + row + column + " input").focus();
-        }
-    }
 
-    if (keypress.code === "Enter") {
-        let answerLetters = answer.split("");
-        let attempt = document.querySelector("#box" + row + "0 input").value + document.querySelector("#box" + row + "1 input").value + document.querySelector("#box" + row + "2 input").value + document.querySelector("#box" + row + "3 input").value + document.querySelector("#box" + row + "4 input").value;
-        let matchedLetters = ["-", "-", "-", "-", "-"];
+        if (keypress.code === "Enter") {
+            let answerLetters = answer.split("");
+            let attempt = document.querySelector("#box" + row + "0 input").value + document.querySelector("#box" + row + "1 input").value + document.querySelector("#box" + row + "2 input").value + document.querySelector("#box" + row + "3 input").value + document.querySelector("#box" + row + "4 input").value;
+            let matchedLetters = ["-", "-", "-", "-", "-"];
 
-        if (attempt === answer) {
-            document.querySelector("#box" + row + "0 input").style.backgroundColor = "rgb(120, 223, 163)";
-            document.querySelector("#box" + row + "1 input").style.backgroundColor = "rgb(120, 223, 163)";
-            document.querySelector("#box" + row + "2 input").style.backgroundColor = "rgb(120, 223, 163)";
-            document.querySelector("#box" + row + "3 input").style.backgroundColor = "rgb(120, 223, 163)";
-            document.querySelector("#box" + row + "4 input").style.backgroundColor = "rgb(120, 223, 163)";
+            if (attempt === answer) {
+                document.querySelector("#box" + row + "0 input").style.backgroundColor = "rgb(120, 223, 163)";
+                document.querySelector("#box" + row + "1 input").style.backgroundColor = "rgb(120, 223, 163)";
+                document.querySelector("#box" + row + "2 input").style.backgroundColor = "rgb(120, 223, 163)";
+                document.querySelector("#box" + row + "3 input").style.backgroundColor = "rgb(120, 223, 163)";
+                document.querySelector("#box" + row + "4 input").style.backgroundColor = "rgb(120, 223, 163)";
 
-            document.querySelector("#av" + row).style.visibility = "visible";
-        }
-        else if (validWords.includes(attempt)) { //checks to see if the word is in the word bank
-            for (let i = 0; i < 5; i++) {
-                if (answerLetters.includes(attempt[i])) {
-                    if (attempt[i] === answerLetters[i]) {
-                        document.querySelector("#box" + row + i + " input").style.backgroundColor = "rgb(120, 223, 163)";
-                        answerLetters.splice(i, 1, "-"); // placeholder hyphen for spliced letter so the # of elements in the array remains the same
-                    }
-                    else {
-                        matchedLetters.splice(i, 1, attempt[i]);
-                    }
-                }
-                else {
-                    document.querySelector("#box" + row + i + " input").style.backgroundColor = "rgb(245, 131, 131)";
-                }
+                document.querySelector("#av" + row).style.visibility = "visible";
+                gameWon = true;
             }
-            for (let i = 0; i < 5; i++) {
-                if (matchedLetters[i].match(/[A-z]/)) { //checks to see if the element is a letter
-                    if (answerLetters.includes(matchedLetters[i])) {
-                        document.querySelector("#box" + row + i + " input").style.backgroundColor = "rgb(255, 255, 204)";
-                        answerLetters.splice(answerLetters.indexOf(matchedLetters[i]), 1, "-");
+            else if (validWords.includes(attempt)) { //checks to see if the word is in the word bank
+                for (let i = 0; i < 5; i++) {
+                    if (answerLetters.includes(attempt[i])) {
+                        if (attempt[i] === answerLetters[i]) {
+                            document.querySelector("#box" + row + i + " input").style.backgroundColor = "rgb(120, 223, 163)";
+                            answerLetters.splice(i, 1, "-"); // placeholder hyphen for spliced letter so the # of elements in the array remains the same
+                        }
+                        else {
+                            matchedLetters.splice(i, 1, attempt[i]);
+                        }
                     }
                     else {
                         document.querySelector("#box" + row + i + " input").style.backgroundColor = "rgb(245, 131, 131)";
                     }
                 }
+                for (let i = 0; i < 5; i++) {
+                    if (matchedLetters[i].match(/[A-z]/)) { //checks to see if the element is a letter
+                        if (answerLetters.includes(matchedLetters[i])) {
+                            document.querySelector("#box" + row + i + " input").style.backgroundColor = "rgb(255, 255, 204)";
+                            answerLetters.splice(answerLetters.indexOf(matchedLetters[i]), 1, "-");
+                        }
+                        else {
+                            document.querySelector("#box" + row + i + " input").style.backgroundColor = "rgb(245, 131, 131)";
+                        }
+                    }
+                }
+                if (row < 5) {
+                    row += 1;
+                    document.querySelector("#av" + (row - 1)).style.visibility = "visible";
+                    document.querySelector("#av" + row).style.visibility = "hidden";
+                    
+                    document.querySelector("#box" + row + "0 input").focus();
+                    column = 0;
+                }
             }
-            if (row < 5) {
-                row += 1;
-                document.querySelector("#av" + (row - 1)).style.visibility = "visible";
-                document.querySelector("#av" + row).style.visibility = "hidden";
+            else { //clears the row
+                document.querySelector("#box" + row + "0 input").value = "";
+                document.querySelector("#box" + row + "1 input").value = "";
+                document.querySelector("#box" + row + "2 input").value = "";
+                document.querySelector("#box" + row + "3 input").value = "";
+                document.querySelector("#box" + row + "4 input").value = "";
+                //resets back to 1st box in row
+                document.querySelector("#box" + row + "0 input").focus();
+                column = 0;
             }
         }
-        else { //clears the row
-            document.querySelector("#box" + row + "0 input").value = "";
-            document.querySelector("#box" + row + "1 input").value = "";
-            document.querySelector("#box" + row + "2 input").value = "";
-            document.querySelector("#box" + row + "3 input").value = "";
-            document.querySelector("#box" + row + "4 input").value = "";
-            //resets back to 1st box in row
-            document.querySelector("#box" + row + "0 input").focus();
-            column = 0;
-        }
+    }
+    // tab, ctrl z + ctrl y need to be fixed
+    else if (keypress.key === "Backspace") {
+        keypress.preventDefault();
     }
 })
 
