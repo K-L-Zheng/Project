ba364ef6e1ce1b0affff5ed1e0f0107f08b15885
Kevin Zheng <90434884+K-L-Zheng@users.noreply.github.com>
2022-05-15 16:10:17 -0400

    Add files via upload

--- Chess/chess.js
+++ Chess/chess.js
@@ -123,7 +123,7 @@ function movePiece(id) {
         castling(id);
         //captures
         if (boardSpace.children.length === 2) {
-            boardSpace.children[0].style.fontSize = "2rem";
+            boardSpace.children[0].style.height = "2rem";
             boardSpace.children[0].style.paddingLeft = "10px";
             //removes the captured piece from the array of remaining pieces
             remainingPieces.splice(remainingPieces.indexOf(boardSpace.children[0].id), 1);
@@ -398,13 +398,13 @@ function enPassant(id) {
     if (turn !== 1 && document.querySelector("#" + lastMovedPiece).classList.contains("moved-once") && document.querySelector("#" + lastMovedPiece).parentElement.id[0] === String.fromCharCode(id.charCodeAt(0))) {
         
         if (players[0] === "light-pc" && id[1] === "6") {
-            document.querySelector("#" + lastMovedPiece).style.fontSize = "2rem";
+            document.querySelector("#" + lastMovedPiece).style.height = "2rem";
             document.querySelector("#" + lastMovedPiece).style.paddingLeft = "10px";
             remainingPieces.splice(remainingPieces.indexOf(lastMovedPiece), 1);
             document.querySelector("#cap-dark-pcs").appendChild(document.querySelector("#" + lastMovedPiece));
         }
         else if (players[0] === "dark-pc" && id[1] === "3") {
-            document.querySelector("#" + lastMovedPiece).style.fontSize = "2rem";
+            document.querySelector("#" + lastMovedPiece).style.height = "2rem";
             document.querySelector("#" + lastMovedPiece).style.paddingLeft = "10px";
             remainingPieces.splice(remainingPieces.indexOf(lastMovedPiece), 1);
             document.querySelector("#cap-light-pcs").appendChild(document.querySelector("#" + lastMovedPiece));
@@ -412,19 +412,22 @@ function enPassant(id) {
     }
 }
 function pawnPromotion(id) {
-    let newPiece = document.createElement("i");
+    let newPiece = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     //checks for piece selection & creates a new piece based on the selection
     switch (true) {
         case document.querySelector("#" + id).classList.contains("queen"):
             document.querySelector("#" + id).parentElement.style.visibility = "hidden";
             promoPieceCount += 1;
 
+            newPiece.setAttribute("viewBox", "0 0 512 512");
+            newPiece.innerHTML = "<path d='M256 112c30.88 0 56-25.12 56-56S286.9 0 256 0S199.1 25.12 199.1 56S225.1 112 256 112zM399.1 448H111.1c-26.51 0-48 21.49-48 47.1C63.98 504.8 71.15 512 79.98 512h352c8.837 0 16-7.163 16-16C447.1 469.5 426.5 448 399.1 448zM511.1 197.4c0-5.178-2.509-10.2-7.096-13.26L476.4 168.2c-2.684-1.789-5.602-2.62-8.497-2.62c-17.22 0-17.39 26.37-51.92 26.37c-29.35 0-47.97-25.38-47.97-50.63C367.1 134 361.1 128 354.6 128h-38.75c-6 0-11.63 4-12.88 9.875C298.2 160.1 278.7 176 255.1 176c-22.75 0-42.25-15.88-47-38.12C207.7 132 202.2 128 196.1 128h-38.75C149.1 128 143.1 134 143.1 141.4c0 18.45-13.73 50.62-47.95 50.62c-34.58 0-34.87-26.39-51.87-26.39c-2.909 0-5.805 .8334-8.432 2.645l-28.63 16C2.509 187.2 0 192.3 0 197.4C0 199.9 .5585 202.3 1.72 204.6L104.2 416h303.5l102.5-211.4C511.4 202.3 511.1 199.8 511.1 197.4z'></path>";
+
             if (players[0] === "light-pc") {
-                newPiece.classList = "fas fa-chess-queen light-pc queen promo";
+                newPiece.classList = "light-pc queen promo";
                 newPiece.id = "light-queen-promo-" + promoPieceCount;
             }
             else {
-                newPiece.classList = "fas fa-chess-queen dark-pc queen promo";
+                newPiece.classList = "dark-pc queen promo";
                 newPiece.id = "dark-queen-promo-" + promoPieceCount;
             }
             
@@ -435,12 +438,15 @@ function pawnPromotion(id) {
             document.querySelector("#" + id).parentElement.style.visibility = "hidden";
             promoPieceCount += 1;
 
+            newPiece.setAttribute("viewBox", "0 0 384 512");
+            newPiece.innerHTML = "<path d='M19 272.5l40.62 18C63.78 292.3 68.25 293.3 72.72 293.3c4 0 8.001-.7543 11.78-2.289l12.75-5.125c9.125-3.625 16-11.12 18.75-20.5L125.2 234.8C127 227.9 131.5 222.2 137.9 219.1L160 208v50.38C160 276.5 149.6 293.1 133.4 301.2L76.25 329.9C49.12 343.5 32 371.1 32 401.5V416h319.9l-.0417-192c0-105.1-85.83-192-191.8-192H12C5.375 32 0 37.38 0 44c0 2.625 .625 5.25 1.75 7.625L16 80L7 89C2.5 93.5 0 99.62 0 106V243.2C0 255.9 7.5 267.4 19 272.5zM52 128C63 128 72 137 72 148S63 168 52 168S32 159 32 148S41 128 52 128zM336 448H47.1C21.49 448 0 469.5 0 495.1C0 504.8 7.163 512 16 512h352c8.837 0 16-7.163 16-16C384 469.5 362.5 448 336 448z'></path>";
+
             if (players[0] === "light-pc") {
-                newPiece.classList = "fas fa-chess-knight light-pc knight promo";
+                newPiece.classList = "light-pc knight promo";
                 newPiece.id = "light-knight-promo-" + promoPieceCount;
             }
             else {
-                newPiece.classList = "fas fa-chess-knight dark-pc knight promo";
+                newPiece.classList = "dark-pc knight promo";
                 newPiece.id = "dark-knight-promo-" + promoPieceCount;
             }
             
@@ -451,12 +457,15 @@ function pawnPromotion(id) {
             document.querySelector("#" + id).parentElement.style.visibility = "hidden";
             promoPieceCount += 1;
 
+            newPiece.setAttribute("viewBox", "0 0 384 512");
+            newPiece.innerHTML = "<path d='M368 32h-56c-8.875 0-16 7.125-16 16V96h-48V48c0-8.875-7.125-16-16-16h-80c-8.875 0-16 7.125-16 16V96H88.12V48c0-8.875-7.25-16-16-16H16C7.125 32 0 39.12 0 48V224l64 32c0 48.38-1.5 95-13.25 160h282.5C321.5 351 320 303.8 320 256l64-32V48C384 39.12 376.9 32 368 32zM224 320H160V256c0-17.62 14.38-32 32-32s32 14.38 32 32V320zM336 448H47.1C21.49 448 0 469.5 0 495.1C0 504.8 7.163 512 16 512h352c8.837 0 16-7.163 16-16C384 469.5 362.5 448 336 448z'></path>";
+
             if (players[0] === "light-pc") {
-                newPiece.classList = "fas fa-chess-rook light-pc rook promo";
+                newPiece.classList = "light-pc rook promo";
                 newPiece.id = "light-rook-promo-" + promoPieceCount;
             }
             else {
-                newPiece.classList = "fas fa-chess-rook dark-pc rook promo";
+                newPiece.classList = "dark-pc rook promo";
                 newPiece.id = "dark-rook-promo-" + promoPieceCount;
             }
             
@@ -467,12 +476,15 @@ function pawnPromotion(id) {
             document.querySelector("#" + id).parentElement.style.visibility = "hidden";
             promoPieceCount += 1;
 
+            newPiece.setAttribute("viewBox", "0 0 320 512");
+            newPiece.innerHTML = "<path d='M272 448h-224C21.49 448 0 469.5 0 496C0 504.8 7.164 512 16 512h288c8.836 0 16-7.164 16-16C320 469.5 298.5 448 272 448zM8 287.9c0 51.63 22.12 73.88 56 84.63V416h192v-43.5c33.88-10.75 56-33 56-84.63c0-30.62-10.75-67.13-26.75-102.5L185 285.6c-1.565 1.565-3.608 2.349-5.651 2.349c-2.036 0-4.071-.7787-5.63-2.339l-11.35-11.27c-1.56-1.56-2.339-3.616-2.339-5.672c0-2.063 .7839-4.128 2.349-5.693l107.9-107.9C249.5 117.3 223.8 83 199.4 62.5C213.4 59.13 224 47 224 32c0-17.62-14.38-32-32-32H128C110.4 0 96 14.38 96 32c0 15 10.62 27.12 24.62 30.5C67.75 106.8 8 214.5 8 287.9z'></path>";
+
             if (players[0] === "light-pc") {
-                newPiece.classList = "fas fa-chess-bishop light-pc bishop promo";
+                newPiece.classList = "light-pc bishop promo";
                 newPiece.id = "light-bishop-promo-" + promoPieceCount;
             }
             else {
-                newPiece.classList = "fas fa-chess-bishop dark-pc bishop promo";
+                newPiece.classList = "dark-pc bishop promo";
                 newPiece.id = "dark-bishop-promo-" + promoPieceCount;
             }
             
